4fc51e2ab8f9afab465c5e1b76d451846c402875
spellbind <2591754854@qq.com>
2023-08-09 01:07:09 +0800

    📝docs：update docs

--- website/config/feature.ts
+++ website/config/feature.ts
@@ -2,18 +2,19 @@ import { IFeature } from "dumi-theme-antd-style";
 
 export const features: IFeature[] = [
   {
-    title: "开箱即用的高质量生态元素",
+    title: "开箱即用",
     description: "包括 物料体系、事件触发器、动作处理器 等",
     link: "/guide",
     image: "📦",
     imageType: "light",
     row: 6,
   },
+
   {
-    title: "可视化",
-    description:"可视化拖放，可视化编辑配置</quotient>",
+    title: "减少开发成本",
+    description:"使用低代码平台，减少开发成本，让复杂的事变简单",
     link: "/guide",
-    image:'🛠️',
+    image:"🚀",
     imageType: "light",
     row: 6,
   },
@@ -28,21 +29,19 @@ export const features: IFeature[] = [
   {
     title: "快速生成",
     description: "快速生成web端 UI 界面",
-    link: "/guide",
     image:"⚡",
     imageType: "light",
     row: 6,
   },
   {
-    title: "减少开发成本",
-    description:"使用低代码平台，减少开发成本，让复杂的事变简单",
-    link: "/guide",
+    title: "可视化操作",
+    description:"可视化拖放，可视化编辑配置",
+    image:'🛠️',
     imageType: "light",
-    image:"🚀",
     row: 6,
   },
   {
-    title: "使用 TypeScript 开发",
+    title: "TypeScript 开发",
     description:"提供完整的类型定义文件",
     image: "💗",
     imageType: "light",
--- website/docs/index.md
+++ website/docs/index.md
@@ -1,9 +1,9 @@
 ---
 hero:
   title: TickTack - <b>Simplify complex things</b>
-  description: ticktack - 将复杂的任务变简单
+  description: ticktack - 让复杂的任务变简单
   actions:
-    - text: 立即开始
+    - text: 立即体验
       link: /guide
     - text: GitHub
       link: 'https://github.com/Magicalboys/tickTack'
