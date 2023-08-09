166ef38b4491fe89da7af5723c9f63b191b43a07
spellbind <2591754854@qq.com>
2023-08-09 16:38:01 +0800

    🐞 fix: fix alias bug

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,6 @@ export default defineConfig({
     alias: {
       "@": resolve(__dirname, "./src") // 路径别名
     },
-    extensions: [".js", ".json", ".ts", "tsx"] // 使用路径别名时想要省略的后缀名，可以自己 增减
+    extensions: [".js", ".json", ".ts", ".tsx"] // 使用路径别名时想要省略的后缀名，可以自己 增减
   }
 });
