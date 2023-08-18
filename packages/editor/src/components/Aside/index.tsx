62b4e79d49f3be2e14095b1dcbe5583064954483
wjywy <2934610933@qq.com>
2023-08-18 19:24:29 +0800

    ✨ feat(editor): 编辑区持续修改

--- packages/editor/src/components/Aside/index.tsx
+++ packages/editor/src/components/Aside/index.tsx
@@ -1,73 +1,78 @@
 import { Fragment, useState } from "react";
-import menu from '../../../i18/CN.json';
-import { jsonToArr } from '../../../util/jsonToObj';
-import Setting from './setting/index';
-import UniversalComponent from './UniversalComponent/index';
-import Code from './code/index';
+import menu from "../../../i18/CN.json";
+import { jsonToArr } from "../../../util/jsonToObj";
+import Setting from "./setting/index";
+import UniversalComponent from "./UniversalComponent/index";
+import Code from "./code/index";
 import "./style.scss";
 
-
 const Aside: React.FC = () => {
-    const list = jsonToArr(menu.MenuSet);
-    const [param, setParam] = useState("UniversalComponent");
+  const list = jsonToArr(menu.MenuSet);
+  const [param, setParam] = useState("UniversalComponent");
 
-    // 函数区
-    const chooseShow = (param: string) => {
-        return (
-                <>
-                {list.map((item) => {
-                    if (param === item.key) {
-                        if (param === "UniversalComponent") {
-                            return (
-                                <>
-                                    <UniversalComponent></UniversalComponent>
-                                </>
-                            )
-                        } else if (param === "Setting") {
-                            return (
-                                <>
-                                    <Setting></Setting>
-                                </>
-                            )
-                        } else if (param === "Code") {
-                            return (
-                                <>
-                                    <Code></Code>
-                                </>
-                            )
-                        }
-                    }
-                })}
-                </>
-        )
-    }
-
-    const chooseParam = (toParam: string) => {
-        console.log(toParam);
-        setParam(toParam);
-    }
-
-    // 组件区
-    const MenuData = list !== null ? list.map((item ,index) => {
-        return (
-            <Fragment key={String(index) + item.value}>
-                <div onClick={() => chooseParam(item.key)} className="container-menu-item">{item.value}</div>
-            </Fragment>
-        )
-    }) : <div>kong</div>
-
+  // 函数区
+  const chooseShow = (param: string) => {
     return (
-        <>
-        <div className="container-aside">
-            <div className="container-menu">
-                {MenuData}
+      <>
+        {list.map((item) => {
+          if (param === item.key) {
+            if (param === "UniversalComponent") {
+              return (
+                <>
+                  <UniversalComponent></UniversalComponent>
+                </>
+              );
+            } else if (param === "Setting") {
+              return (
+                <>
+                  <Setting></Setting>
+                </>
+              );
+            } else if (param === "Code") {
+              return (
+                <>
+                  <Code></Code>
+                </>
+              );
+            }
+          }
+        })}
+      </>
+    );
+  };
+
+  const chooseParam = (toParam: string) => {
+    console.log(toParam);
+    setParam(toParam);
+  };
+
+  // 组件区
+  const MenuData =
+    list !== null ? (
+      list.map((item, index) => {
+        return (
+          <Fragment key={String(index) + item.value}>
+            <div
+              onClick={() => chooseParam(item.key)}
+              className='container-menu-item'
+            >
+              {item.value}
             </div>
-            <div>
-                {chooseShow(param)}
-            </div>
-        </div>
+          </Fragment>
+        );
+      })
+    ) : (
+      <div>kong</div>
+    );
+
+  return (
+    <>
+      <div className='container-aside'>
+        <div className='container-menu'>{MenuData}</div>
+        <div>{chooseShow(param)}</div>
+      </div>
     </>
-    )
-}
+  );
+};
 
 export default Aside;
